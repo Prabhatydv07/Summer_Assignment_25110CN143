0e26a82c257687b6229009e0c7c6a236dce7b89e
Prabhat Yadav <heyitser.prabhatyadav@gmail.com>
2026-06-09 09:56:15 +0530

    Day-9 Uploaded

---
+++ Day-9/Program34.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    printf("Enter the number of rows :");
+    scanf("%d", &n);
+
+    for(int i = 0; i<n; i++){
+        int count = 1;
+        for(int j = n; j>i ; j--){
+            printf("%d ", count);
+            count++;
+        }
+        printf("\n");
+    }
+}
