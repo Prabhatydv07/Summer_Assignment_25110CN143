98c5ff1ee9b15ba6b7638ed0bdd1a9bb9f35be9e
Prabhat Yadav <heyitser.prabhatyadav@gmail.com>
2026-06-16 16:15:12 +0530

    Last program

--- Day-16/Program64.c
+++ Day-16/Program64.c
@@ -1,22 +1,38 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    int n;
-    printf("Enter the length of the array : ");
-    scanf("%d", &n);
+int removeDuplicates(int arr[], int n) {
+    if (n == 0 || n == 1)
+        return n;
 
-    printf("Enter the elements of the array - \n");
-    int arr[n];
-    for(int i = 0; i<n; i++){
-        scanf("%d", &arr[i]);
+    int temp[n];
+    int j = 0;
+
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i] != arr[i + 1])
+            temp[j++] = arr[i];
     }
+    temp[j++] = arr[n - 1];
 
-    for(int i = 0; i<n; i++){
-        for(int j = i; j<n; j++){
-            if(arr[j] == arr[i]){
-                arr[i] = arr[i+1];
-            }
-        }
-    }
+    for (int i = 0; i < j; i++)
+        arr[i] = temp[i];
+
+    return j;
+}
+
+int main() {
+    int arr[] = {1, 2, 2, 3, 4, 4, 4, 5, 6, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array:  ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    n = removeDuplicates(arr, n);
+
+    printf("\nAfter removing dublicate elements");
+        for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    printf("\n");
     return 0;
 }
