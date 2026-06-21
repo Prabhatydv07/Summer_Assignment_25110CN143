232660243ac50a0cf44f5e32436cafe77f33b65c
Prabhat Yadav <heyitser.prabhatyadav@gmail.com>
2026-06-21 21:38:35 +0530

    Minor Changes

--- Day-18/Program71.c
+++ Day-18/Program71.c
@@ -5,30 +5,35 @@ int main(){
     printf("Enter the length of the array : ");
     scanf("%d", &n);
     int arr[n];
-
-    printf("Enter the elements of the array : ");
+    printf("Enter the elements of the array:");
     for(int i = 0; i<n; i++){
         scanf("%d", &arr[i]);
     }
 
-    int search;
-    printf("Enter the element to search : ");
-    scanf("%d", &search);
+    int target ;
+    printf("Enter the element to find : ");
+    scanf("%d", &target);
+    int indx = 0, found = 0;
 
-    int flag = 0,indx = 0;
-    for(int i = 0; i<n;i++){
-        if(arr[i] == search){
-            flag = 1;
-            indx = i;
+    int lo = 0, hi = n-1;
+    while(lo<=hi){
+        int mid = (lo + hi) /2;
+        if(arr[mid]<target) lo = mid + 1;
+        else if(arr[mid]>target) hi = mid -1;
+        else{
+            indx = mid;
+            found = 1;
+            break;
         }
     }
 
-    if(flag == 1){
-        printf("The element fond in array at %d postion !!",indx);
+    if(found){
+        printf("The element found at array at %d index", indx);
     }
 
     else{
-        printf("The element is not presnt in the array");
+        printf("The element is not present in the array");
     }
+
     return 0;
 }
